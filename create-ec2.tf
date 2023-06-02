3858d99939180d25b39dd3651b315841384dbed7
Pratap Sharma <prs@logpoint.com>
2023-06-02 15:02:01 +0545

    Updated the terraform script and added gitignore

--- create-ec2.tf
+++ create-ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-14c5486b"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "example-instance"
